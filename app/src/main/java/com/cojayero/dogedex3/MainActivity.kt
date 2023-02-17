2e5ac0308a09885832ba7e4c80bb84f800a0b362
Baldomero Fernandez <f_baldomero@hotmail.com>
2023-02-17 20:16:57 +0100

    Initial commit

---
+++ app/src/main/java/com/cojayero/dogedex3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cojayero.dogedex3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
