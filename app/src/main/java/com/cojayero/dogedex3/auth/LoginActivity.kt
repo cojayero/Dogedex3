d4cfbe7192387b756157a05d604633bb8bd16bd8
Baldomero Fernandez <f_baldomero@hotmail.com>
2023-03-06 21:54:12 +0100

    Implementada la navegacion desde el fragmento Login al register. Ojo, daa error al poner ClassCast proque importó la de Java

--- app/src/main/java/com/cojayero/dogedex3/auth/LoginActivity.kt
+++ app/src/main/java/com/cojayero/dogedex3/auth/LoginActivity.kt
@@ -2,14 +2,20 @@ package com.cojayero.dogedex3.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.cojayero.dogedex3.R
 import com.cojayero.dogedex3.auth.ui.main.LoginFragment
+import com.cojayero.dogedex3.auth.ui.main.LoginFragmentDirections
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginFragment.LoginFragmentActions {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+    }
+
+    override fun onRegisterButtonClick() {
+        findNavController(R.id.nav_host_fragment).navigate(LoginFragmentDirections.actionLoginFragmentToSignUpFragment())
     }
 }
--- app/src/main/java/com/cojayero/dogedex3/auth/ui/main/LoginFragment.kt
+++ app/src/main/java/com/cojayero/dogedex3/auth/ui/main/LoginFragment.kt
@@ -1,23 +1,36 @@
 package com.cojayero.dogedex3.auth.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.cojayero.dogedex3.R
 import com.cojayero.dogedex3.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    interface LoginFragmentActions {
+        fun onRegisterButtonClick()
+    }
 
-
+    private lateinit var loginFragmentActions: LoginFragmentActions
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        loginFragmentActions = try {
+            context as LoginFragmentActions
+        } catch (e: ClassCastException) {
+            throw ClassCastException("$context must implement LoginFragmentActions")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentLoginBinding.inflate(inflater)
+        binding.loginRegisterButton.setOnClickListener {
+            loginFragmentActions.onRegisterButtonClick()
+        }
         return binding.root
     }
 
