35d0867a2c6a43e895842c271b06788186da650a
Baldomero Fernandez <f_baldomero@hotmail.com>
2023-03-22 16:30:08 +0100

    implementado el clasificador de imágenes

--- app/src/main/java/com/cojayero/dogedex3/Constants.kt
+++ app/src/main/java/com/cojayero/dogedex3/Constants.kt
@@ -8,3 +8,4 @@ const val SIGN_UP_URL = "sign_up"
 const val SIGN_IN_URL = "sign_in"
 const val ADD_DOG_TO_USER = "add_dog_to_user"
 const val GET_USER_DOGS_URL ="get_user_dogs"
+const val MAX_RECOGNITION_DOG_RESULTS = 5
---
+++ app/src/main/java/com/cojayero/dogedex3/machinelearning/DogRecognition.kt
@@ -0,0 +1,3 @@
+package com.cojayero.dogedex3.machinelearning
+
+class DogRecognition (val id:String, val confidence : Float)
